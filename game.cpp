eb79a7820e4dbf1659330048782a310e73c793bb
Richard Phan <richard.phan626@gmail.com>
2025-10-05 08:40:10 -0700

    Created 2 functions
    1) function to display title
    2) function to display welcome message

--- game.cpp
+++ game.cpp
@@ -9,12 +9,14 @@
 using namespace std;
 
 //Function Prototypes
+void displayTitle();
+void displayIntro();
 
 int main() {
 	//Welcome user
-
+	displayTitle();
 	//Introduce the game
-
+	displayIntro();
 	//ask for user name - const variable
 
 
@@ -23,7 +25,19 @@ int main() {
 
 //Functions
 
-//Display Title / introductions
+//Prints title of the game
+void displayTitle() {
+	cout << "===== Welcome to Chronicles of a Cursed Crown ===== " << endl << endl; //FIXME \n not registered by VS studio
+}
+//Welcome message
+void displayIntro() {
+	cout << "Hello Player!" << endl;
+	cout << "You have been summoned to investigate the monster calamity plauging this world." << endl;
+	cout << "As you embark on your journey, you'll encounter many different kinds of people, challenges, and enemies." << endl;
+	cout << "Please start by entering your name and selecting your class: " << endl << endl;
+
+
+}
 
 //character class with variables HP, strength, MP
 
