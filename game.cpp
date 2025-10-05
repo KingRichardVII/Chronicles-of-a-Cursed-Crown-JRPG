0d1c27f08f0af147fa191ac62ee2d5b20eaad234
Richard Phan <richard.phan626@gmail.com>
2025-10-05 08:20:01 -0700

    Code outline

---
+++ game.cpp
@@ -0,0 +1,34 @@
+/*
+	Richard Phan
+
+	This program is a simple, turn based JRPG game 
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Function Prototypes
+
+int main() {
+	//Welcome user
+
+	//Introduce the game
+
+	//ask for user name - const variable
+
+
+	return 0;
+}
+
+//Functions
+
+//Display Title / introductions
+
+//character class with variables HP, strength, MP
+
+//Class skills
+
+//Store w purchasable items
+
+//function to mimick battle sequence (main function)
